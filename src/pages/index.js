e9065e007beefd7ee40c99bff27ca712d7235baa
yaMertvec <yamertvec@mail.ru>
2023-02-04 15:39:27 +0300

    fix:исправили ошибки указанные ревьюером

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,13 +16,7 @@ const formAddElement = document.querySelector('.popup__add');
 const nameInput = formEditElement.querySelector('.popup__input_data_name');
 const jobInput = formEditElement.querySelector('.popup__input_data_job');
 const openAddCardButton = document.querySelector('.profile__add-button');
-
 const listElements = document.querySelector('.elements');
-
-
-
-
-
 
 const createCard = (item) => {
   const card = new Card(item, `#card-template`, handleOpenImagePopup)
@@ -30,9 +24,6 @@ const createCard = (item) => {
   return newCard
 }
 
-initialCards.forEach((item) => {
-  listElements.prepend(createCard(item));
-});
 
 const cardList = new Section({
   items: initialCards,
@@ -42,6 +33,8 @@ const cardList = new Section({
   },
 }, '.elements'
 );
+
+cardList.renderItems(initialCards);
 
 const popupImage = new PopupWithImage('.popup_image');
 popupImage.setEventListeners();
@@ -66,7 +59,6 @@ const userInfo = new UserInfo({
 const popupProfile = new PopupWithForm('.popup_type-edit', handleFormEditSubmit);
 popupProfile.setEventListeners();
 
-
 const popupCard = new PopupWithForm('.popup_card-add', handleCardFormSubmit)
 popupCard.setEventListeners();
 
@@ -74,7 +66,6 @@ function handleFormEditSubmit(formValues) {
   userInfo.setUserInfo(formValues);
   popupProfile.close();
 }
-
 
 popupOpenProfileEdit.addEventListener('click', function () {
   const userData = userInfo.getUserInfo()
@@ -84,18 +75,15 @@ popupOpenProfileEdit.addEventListener('click', function () {
   popupProfile.open()
 })
 
-
 //--------------------открываем и закрываем попап с добавлением карточки ---------------//
 openAddCardButton.addEventListener('click', () => {
   popupCardFormValidator.resetValidation();
   popupCard.open();
 })
 
-
 const popupProfileFormValidator = new FormValidator(config, popupProfileEdit);
 popupProfileFormValidator.enableValidation();
-popupProfileFormValidator.resetValidation()
 
 const popupCardFormValidator = new FormValidator(config, formAddElement);
 popupCardFormValidator.enableValidation();
-popupCardFormValidator.resetValidation()
+
