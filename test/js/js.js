b50daad95cd1af7d1a7f3274afb43494b8556efc
yaMertvec <yamertvec@mail.ru>
2023-02-18 05:22:33 +0300

    Revert "сделали пр9"
    This reverts commit 6abb9501ce4e634412e3664086af0e05d354ed6e.

--- test/js/js.js
+++
@@ -1,9 +0,0 @@
-const ul = document.querySelector('ul');
-const template = document.querySelector('#element-li');
-const span = template.content.querySelector('span');
-span.textContent = "cheburek";
-let li = template.content.cloneNode(true);
-ul.append(li);
-span.textContent = "12212121";
-li = template.content.cloneNode(true);
-ul.append(li);
